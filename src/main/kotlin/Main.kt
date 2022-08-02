c0ac98073a721c2bb104170ad1ce5a555771a961
Atlapaltin <108687053+Atlapaltin@users.noreply.github.com>
2022-08-02 19:31:46 +0300

    Amended as per teacher's comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
-fun main(args: Array<String>) {
-    var likes = 1
-    if ((likes % 10) == 1 && (likes % 100) !=11) println ("Понравилось $likes человеку") else println ("Понравилось $likes людям")
+fun main() {
+    val likes = 79
+    val result = if ((likes % 10) == 1 && (likes % 100) !=11) ("Понравилось $likes человеку")
+    else ("Понравилось $likes людям")
+        println (result)
 }
